08d9d635720c492cb7c879e8836f6d27bb7c54ad
Pamela Cortez <77471006+pamicortez@users.noreply.github.com>
2024-02-16 00:41:24 -0300

    final

--- data.jl
+++ data.jl
@@ -19,7 +19,7 @@ function updateResidualSupply(C, DP, t, residualsupply, loads)
                             residualsupply[c,t2] = 0
                         end
                         auxs = 0
-                    else#if residualsupply[c,t2] > ZERO
+                    elseif residualsupply[c,t2] > 0
                         auxs -= residualsupply[c,t2]
                         residualsupply[c,t2] = 0
                     end
@@ -40,17 +40,18 @@ function updateResidualDemand(C, DP, t, direction, residualdemand, loads)
                     if auxd < ZERO
                         break
                     end
+                    # print(c, " ", j, " ", t2, " ", auxd, " ", residualdemand[c,j,t2])
                     if residualdemand[c,j,t2] >= auxd
                         residualdemand[c,j,t2] -= auxd #trunc(Int, auxd + ZERO)
                         if residualdemand[c,j,t2] > -ZERO && residualdemand[c,j,t2] < ZERO
                             residualdemand[c,j,t2] = 0
                         end
                         auxd = 0
-                    else#if residualdemand[c,j,t2] > ZERO
+                    elseif residualdemand[c,j,t2] > 0
                         auxd -= residualdemand[c,j,t2]
                         residualdemand[c,j,t2] = 0
                     end
-                    # println(c, " ", j, " ", t2, " ", residualdemand[c,j,t2])
+                    # println(" ", auxd, " ", residualdemand[c,j,t2])
                 end
             end
         end
@@ -60,7 +61,6 @@ end
 function getResidual(t, loads)
     residualsupply = copy(supply)
     residualdemand = copy(demand)
-
     for taux in 1:t-1
         updateResidual(C, DP, taux, residualsupply, residualdemand, loads)
     end
@@ -161,11 +161,11 @@ function getRoutingvars(m, k, t, v, q)
         for i in No
             for j in No
                 if getvalue(m[:V][i,j,p]) > ZERO
-                    push!(v, (i,j,k,t,p+10))
+                    push!(v, (i,j,k,t,p))
                     # if DEBUG print("V ", k, " ", t, " ", p, " (", i, " -> ", j, ")") end
                     if i != "theta" && j != "theta"
                         for c in C
-                            q[c,i,j,k,t,p+10] = getvalue(m[:Q][c,i,j,p])
+                            q[c,i,j,k,t,p] = getvalue(m[:Q][c,i,j,p])
                             # if DEBUG @printf("\t%s = %.2f\t ", c, q[c,i,j,k,t,p]) end
                         end
                     end
@@ -200,19 +200,9 @@ end
 
 function getDPsortedByPrio(prio, dday)
     aux = copy(DP)
-    i = 1
-    prioaux = Array{Float64}(undef, nDP)
-    for j in aux
-        prioaux[i] = sum(dday[c,j] for c in C)
-        i += 1
-    end
+    prioaux = [sum(dday[c,j] for c in C) for j in aux]
     aux = aux[sort([1:nDP;], by=i->(prioaux[i]), rev=true)]
-
-    i = 1
-    for j in aux
-        prioaux[i] = sum(prio[c,j] for c in C)
-        i += 1
-    end
+    prioaux = [sum(prio[c,j] for c in C) for j in aux]
     return aux[sort([1:nDP;], by=i->(prioaux[i]), rev=true)]
 end
 
@@ -225,13 +215,18 @@ function getEquity(demand1)
             for j in DP
                 for j2 in DP
                     if sum(demand[c,j,tau] for tau in t1:t) > ZERO && sum(demand[c,j2,tau] for tau in t1:t) > ZERO
-                        eq[c,t] = max(eq[c,t], sum(demand1[c,j,tau] for tau in t1:t)/sum(demand[c,j,tau] for tau in t1:t)
-                            - sum(demand1[c,j2,tau] for tau in t1:t)/sum(demand[c,j2,tau] for tau in t1:t))
-                        # println(sum(demand1[c,j,tau] for tau in t1:t), " ", sum(demand[c,j,tau] for tau in t1:t), " ",
-                        #    sum(demand1[c,j2,tau] for tau in t1:t), " ", sum(demand[c,j2,tau] for tau in t1:t))
+                        aux = sum(demand1[c,j,tau] for tau in t1:t)/sum(demand[c,j,tau] for tau in t1:t)
+                            - sum(demand1[c,j2,tau] for tau in t1:t)/sum(demand[c,j2,tau] for tau in t1:t)
+                        eq[c,t] = max(eq[c,t], aux)
+
+                        if aux > 1
+                            println(j, " ", sum(demand1[c,j,tau] for tau in t1:t), " ", sum(demand[c,j,tau] for tau in t1:t), " ",
+                                j2, " ", sum(demand1[c,j2,tau] for tau in t1:t), " ", sum(demand[c,j2,tau] for tau in t1:t), " ", aux)
+                        end
                     end
                 end
             end
+            # println(c, " ", t, " ", eq[c,t])
         end
     end
 
@@ -328,8 +323,9 @@ function updateSupply(t, totalincrease)
     # println(t, " updateSupply ", totalincrease)
 end
 
-function getDataUpdate(t, updated)
+function getDataUpdate(t, q)
     filename = string(filepath, instance, "day", t, ".txt")
+    updated = false
     if isfile(filename)
         local aux = readdlm(filename, '$')
         for i in 1:trunc(Int, length(aux)/3)
@@ -340,15 +336,51 @@ function getDataUpdate(t, updated)
             r = aux[i,2]
             howmuch = aux[i,3]
             # println("demand ", demand["cleaning kit",j,1])
-            updateDemand(t, j, r, howmuch)
-            # println("demand2 ", demand["cleaning kit",j,1])
+            if r < 0 && t > 1
+                already_delivered = Dict()
+                for c in C
+                    already_delivered[c] = 0
+                end
+                for (c,i,j2,k,t2,p) in keys(q)
+                    if j2 == j && t2 in div(t-1,threshold[c]+1)*(threshold[c]+1)+1:t-1
+                        already_delivered[c] += q[c,i,j,k,t2,p]
+                    end
+                end
+
+                for c in C
+                    t1 = div(t-1,threshold[c]+1)*(threshold[c]+1)+1
+                    if t1 <= t-1
+                        howmuch = min(howmuch, sum(demand[c,j,t2] for t2 in t1:t-1) - already_delivered[c])
+                    end
+                end
+                # println(j, " ", t, " ", howmuch, " ", already_delivered)
+            else
+                # println(j, " ", t, " ", r, " ", howmuch)
+            end
+            if howmuch > 0
+                updateDemand(t, j, r, howmuch)
+                updated = true
+            end
         end
 
         # println("supply ", supply["cleaning kit",1])
-        updateSupply(t, length(aux) > 1 ? aux[trunc(Int, length(aux)/3),1] : aux[1])
+        saux = length(aux) > 1 ? aux[trunc(Int, length(aux)/3),1] : aux[1]
+        if saux > 0
+            updateSupply(t, saux)
+            updated = true
+        end
         # println("supply2 ", supply["cleaning kit",1])
-    else
-        if isempty(updated) || t == tend["food kit",t]
+    end
+    updated2 = readPie(string(filepath, instance, "pied", t, ".txt"))
+    return updated || updated2
+end
+
+function setDataUpdate()
+    updated = Dict()
+    totalincrease = ceil(Int, total_demand*(1-1/psupply))
+    for t in 2:13
+        filename = string(filepath, instance, "day", t, ".txt")
+        if isempty(updated) || t == tend["food kit",t-1]+1
             for j in DP
                 updated[j] = false
             end
@@ -356,40 +388,45 @@ function getDataUpdate(t, updated)
 
         open(filename, "w") do f
         # ----------------------------------------------  demand ---------------------------------------------- #
-            totalincrease = 0
+            nnodes = 0
             for j in DP
-                if !updated[j] && rand() < 0.3 # j can only be updated once per food kit period
+                if !updated[j] && rand() < (15-t)/15 && nnodes < nDP/4# j can only be updated once per food kit period
                     r = rand(-10:20)/100 # how much
                     c = "food kit"
                     ti = div(t-1,threshold[c]+1)*(threshold[c]+1)+1
                     howmuch = ceil(Int, abs(r)*sum(demand[c,j,t2] for t2 in ti:t))
 
                     if howmuch > 1
-                        updateDemand(t, j, r, howmuch)
+                        nnodes += 1
+                        updated[j] = true
                         totalincrease += r > 0 ? howmuch : -howmuch
                         r = r > 0 ? 1 : -1
                         write(f, "$j\$$r\$$howmuch\n")
-                        # println(t, " ", j, " ", r, " ", sum(demand[c,j,t2] for t2 in ti:t), " ", howmuch)
+                        println(t, " ", j, " ", r, " ", sum(demand[c,j,t2] for t2 in ti:t), " ", howmuch)
                     end
                 end
             end
 
         # ----------------------------------------------  supply ---------------------------------------------- #
-            if totalincrease > 1 && rand() < 0.5
-                r = rand(30:100)/100 # how much
-                totalincrease = ceil(Int, totalincrease*r)
-                updateSupply(t, totalincrease)
-                write(f, "$totalincrease\n")
+            r = rand()
+            if (t != 11 && t != 12) && totalincrease > 0 && r < t/12
+                aux = ceil(Int, totalincrease * (t == 10 || t == 13 ? 1 : r))
+                totalincrease -= aux
+                if totalincrease < ZERO
+                    totalincrease = 0
+                end
+                println(t, " ", r, " ", aux, " ", totalincrease)
+                write(f, "$aux\n")
             else
                 write(f, "0\n")
             end
-            # println(t, " ", totalincrease)
         end
     end
 
     # ----------------------------------------------  pi ---------------------------------------------- #
-    filename = string(filepath, instance, "pied", t, ".txt")
-    if !readPie(filename)
+    for t in 2:13
+        filename = string(filepath, instance, "pied", t, ".txt")
+        # if !readPie(filename)
         up = false
         for i in N
             for j in N
