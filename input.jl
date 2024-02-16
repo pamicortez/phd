8da508d42bfe8cbdff8ac4efc2b6789820c34c06
Pamela Cortez <77471006+pamicortez@users.noreply.github.com>
2024-02-16 00:50:58 -0300

    final

--- input.jl
+++ input.jl
@@ -1,41 +1,54 @@
-using JuMP, CPLEX, DataStructures, DelimitedFiles, Statistics, Printf#, MathOptFormat
+using JuMP, CPLEX, DataStructures, DelimitedFiles, Statistics, MathOptInterface, Combinatorics
 
-const EXACT = 1
-const CG = 2
+const B = 100000000
+const ZERO = 0.0001
+const NTHREADS = 8
+const SLEEP = 1
+
+const CHONLY = 0
+const RELAXFIX = 1
+const FIXOPT = 2
 const PS = 3
-const KNOWFUTURE = false
+const EXACT = 4
+const DC = 5
+const GRASP = 6
+# const PETAL = 7
+
+const ITUPDATE = 100
+alpha = 0.0
 
 const BYPRIORITY = 1
 const BYDEMAND = 2
 
-const B = 1000000
-const ZERO = 0.0001
-const NTHREADS = 8
-const SLEEP = 10
-const PERCENTAGE = 10
-const PERIODS_PS = 3
-
 instance = ARGS[1]
-# const METHOD = parse(Int, ARGS[3])
-# const FIRSTCHOICE = parse(Int, ARGS[4])
-# const PSEQ = parse(Bool, ARGS[5])
-# const nV = parse(Float64, ARGS[6])
-# cities = readdlm("/home/pcandida/punim0040/instances/cidadesSP.txt")
-# auxDistance = readdlm("/home/pcandida/punim0040/instances/distanciasSP.txt")/1000
-# filepath = "/home/pcandida/punim0040/instances/deterministic"
-#instance = "151"
-const METHOD = PS
-const FIRSTCHOICE = BYPRIORITY
-const PSEQ = true
-const nV = parse(Int, instance) > 5 ? 2.5 : 3.5
-cities = readdlm("/Users/pcandida/Dropbox/unimelb/planilhas/brasil/cidadesSP.txt")
-auxDistance = readdlm("/Users/pcandida/Dropbox/unimelb/planilhas/brasil/distanciasSP.txt")/1000
-filepath = "/Users/pcandida/Dropbox/unimelb/instancesBRd/"
-
-println(filepath, " ", instance, " ", METHOD, " ", FIRSTCHOICE, " ", PSEQ, " ", nV)
+const ninstance = parse(Int, instance)
+const METHOD = parse(Int, ARGS[2])
+const RUN_LS = false #parse(Bool, ARGS[7])
+const RERUN_LS = false # compared, makes no difference
+const FIRSTCHOICE = parse(Int, ARGS[3])
+const EQCONSTR = parse(Bool, ARGS[4])
+const psupply = parse(Float64, ARGS[5]) 
+const pvehicles = parse(Int, ARGS[6])
+const multivehicle = true #parse(Bool, ARGS[7])
+const multicommodity = true # parse(Bool, ARGS[8])
+const roadpi = true # parse(Bool, ARGS[9])
 const DEBUG = true #instance == "67"
+const KNOWFUTURE = false #parse(Bool, ARGS[8])
+const DYNAMIC_ANALYSIS = false #parse(Bool, ARGS[9])
+const wfairness = parse(Int, ARGS[7])
+const wtransp = parse(Int, ARGS[8])
 
-const o = "BAURU, SP"#["APIAI, SP", "BAURU, SP", "PRESIDENTE PRUDENTE, SP", "SAO PAULO, SP", "REGISTRO, SP", "RIBEIRAO PRETO, SP", "TAUBATE, SP"]# Array{String}(readdlm(string(filepath, instance, "FN.txt"))[:,1]) # candidates
+cities = readdlm("/home/pcandida/punim0040/instances/cidadesSP.txt")
+coordinates = readdlm("/home/pcandida/punim0040/instances/coordenadasSP.txt")
+auxDistance = readdlm("/home/pcandida/punim0040/instances/distanciasSP.txt")/1000
+filepath = "/home/pcandida/punim0040/instances/deterministic/"
+# cities = readdlm("/Users/pcandida/Dropbox/unimelb/planilhas/brasil/cidadesSP.txt")
+# coordinates = readdlm("/Users/pcandida/Dropbox/unimelb/planilhas/brasil/coordenadasSP.txt")
+# auxDistance = readdlm("/Users/pcandida/Dropbox/unimelb/planilhas/brasil/distanciasSP.txt")/1000
+# filepath = "/Users/pcandida/Dropbox/unimelb/instancesBRd/"
+
+#["APIAI, SP", "BAURU, SP", "PRESIDENTE PRUDENTE, SP", "SAO PAULO, SP", "REGISTRO, SP", "RIBEIRAO PRETO, SP", "TAUBATE, SP"]# Array{String}(readdlm(string(filepath, instance, "FN.txt"))[:,1]) # candidates
+const o = ninstance < 190 ? "BAURU, SP" : "SAO PAULO, SP"
 populationDP = readdlm(string(filepath, instance, "DP.txt"))
 DP = Array{String}(populationDP[:,1])
 populationDP = Array{Integer}(populationDP[:,2])
@@ -45,25 +58,24 @@ const No = ["theta"; N]
 nDP = length(DP)
 n = nDP + 1
 
-const IMPROVEMENT = 0.98
-const IMPROVEMENTB = 0.70
-const MAXARCS = nDP < 26 ? 36 : 18
-# const TL = nDP < 26 ? 20 : 40
-const TIME_LIMIT = nDP * (nDP+1) / 2  #testar tb com 30s
+const IMPROVEMENT = 0.99
+# const IMPROVEMENTB = 0.70
+const TL = 4
+MY_TIME_LIMIT = (div(nDP * (nDP+1), 3) + 30)# / (METHOD == GRASP ? TL : 3) #(ninstance > 15 && ninstance < 200) || (ninstance > 215) ? 4 : 2)
+const WINDOW = 2
+const PERCENTAGE = ninstance < 190 ? 10 : 3
+println(filepath, " ", instance, " ", PERCENTAGE, "% supply: ", psupply, " vehicles: ", pvehicles, " CH: ", FIRSTCHOICE, " IH: ", METHOD, " eq: ", EQCONSTR, " future: ", KNOWFUTURE, " time limit: ", MY_TIME_LIMIT)
 
-distance = Dict{Any,Float64}()
+coordinate = Dict{Any,Tuple{Float64,Float64}}()
 for i in 1:645
-    for j in 1:645
-        if cities[i] in N && cities[j] in N
-            distance[cities[i],cities[j]] = auxDistance[i,j]
-        end
+    if coordinates[i,1] in N
+        coordinate[coordinates[i,1]] = (coordinates[i,2],coordinates[i,3])
     end
 end
-max_distance = maximum(auxDistance)
 
-const nC = 3
+const nC = multicommodity ? 3 : 1
 const L = ["relief itens", "food"]
-const C = ["food kit", "cleaning kit", "mattress"]
+const C = multicommodity ? ["food kit", "cleaning kit", "mattress"] : ["food kit"]
 # const compatibility = Dict{Any,Int8}()
 # compatibility["food kit", "relief itens"] = 1
 # compatibility["cleaning kit", "relief itens"] = 1
@@ -71,7 +83,8 @@ const C = ["food kit", "cleaning kit", "mattress"]
 # compatibility["food kit", "food"] = 1
 # compatibility["cleaning kit", "food"] = 1
 # compatibility["mattress", "food"] = 1
-const priority = Dict{String,Integer}("food kit" => 15, "cleaning kit" => 2, "mattress" => 3)
+const priority = Dict{String,Integer}("food kit" => 7, "cleaning kit" => 2, "mattress" => 3)
+const priorityo = Dict{String,Integer}("food kit" => 15, "cleaning kit" => 2, "mattress" => 3)
 const weight = Dict{String,Float64}("food kit" => 5, "cleaning kit" => 1, "mattress" => 3)
 # const volume = Dict{String,Float64}("food kit" => 0.001, "cleaning kit" => 0.001, "mattress" => 0.1)
 
@@ -108,32 +121,16 @@ const peopleDP = Dict{String,Integer}()
 for j in 1:nDP
     peopleDP[DP[j]] = trunc(Integer, populationDP[j]*PERCENTAGE/100)
 end
-const total_demand = sum(values(peopleDP))
-# println("% of people affected = ", PERCENTAGE, ", number of people affected = ", total_demand)
-
-const supply = Dict{Any,Float64}()
-const demand = Dict{Any,Float64}()
-const urgentdemand = Dict{Any,Float64}()
-for t in T
-    supply["food kit",t]     = (t-1) % 5 == 0 ? trunc(Integer, total_demand) : 0
-    supply["cleaning kit",t] = t == 1 || t == 11 ? trunc(Integer, total_demand/4) : 0
-    supply["mattress",t]     = t == 1 ? trunc(Integer, total_demand) : 0
-
-    for (j,pop) in peopleDP
-        urgentdemand["food kit",j,t]     = (t-1) % 5 == 0 ? pop : 0
-        urgentdemand["cleaning kit",j,t] = t == 1 || t == 11 ? trunc(Integer, pop/4) : 0
-        urgentdemand["mattress",j,t]     = t == 1 ? pop : 0
-        demand["food kit",j,t]     = urgentdemand["food kit",j,t]
-        demand["cleaning kit",j,t] = urgentdemand["cleaning kit",j,t]
-        demand["mattress",j,t]     = urgentdemand["mattress",j,t]
-    end
-end
+const total_demand = psupply * PERCENTAGE/100 * sum(populationDP)
+supply, demand = getSupplyDemand()
+println("% of people affected = ", PERCENTAGE, ", number of people affected on first week = ", sum(demand[C[1],j,t] for j in DP for t in 1:5),
+    ", supply = ", sum(supply[C[1],t] for t in 1:5))
 
 # capacity FN
 # http://www.logcluster.org/blog/logistics-cluster-bolsters-ongoing-humanitarian-response-fiji-mobile-storage-units
 # MSU is 24x10x5 meters, or a total of 240 m², with a capacity to store 840 cubic meters of food and Non Food Items (NFIs).
 # 840 m3 = 70% of 1200 m3
-const capacityF = 840
+# const capacityF = 840
 # opening costs - not used
 
 # H-36 Caracal
@@ -147,26 +144,29 @@ const capacityF = 840
 # Volkswagen Delivery 8.160 Mercedes-Benz Sprinter 415 CDI
 # https://www.noticiasautomotivas.com.br/sprinter/
 # 15-16 mpg $1 diesel/L
+# caminhão toco https://blog.texaco.com.br/ursa/tipos-de-caminhoes-e-capacidades/
+const unloadtime = 0.25
 const K = Array{String}(undef,0)
 const nvehicles = Dict{Any,Integer}()
 const vehicle = Dict{Any,Bool}()
-const vtypes = ["MB", "VW"]
+const vtypes = multivehicle ? ["MB", "VW"] : ["toco"] #"MB", "VW"]
 const vmodel = Dict{String,String}()
-const vload = Dict{String,Integer}("MB" => 2040, "VW" => 8150)#, "helicopter" => 5406
-const vloadtime = Dict{String,Float16}("MB" => 0.7, "VW" => 1.5, "helicopter" => 0.5)
-const vcost = Dict{String,Integer}("MB" => 100, "VW" => 280, "helicopter" => 1000) # R$ per day
-const vfuel = Dict{String,Float64}("MB" => 2.1016*0.12, "VW" => 2.1016*0.15, "helicopter" => 6.31) # R$ per km
-const vspeed = Dict{String,Float64}("MB" => 70, "VW" => 70) # km/h
+const vload = Dict{String,Integer}(multivehicle ? ("MB" => 2000, "VW" => 8150) : "toco" => 7000)
+const vloadtime = Dict{String,Float16}(multivehicle ? ("MB" => 0.25, "VW" => 0.6) : "toco" => 0.5) # per trip, not per stop, "helicopter" => 0.5
+const vcost = Dict{String,Integer}(multivehicle ? ("MB" => 200, "VW" => 300) : "toco" => 200) # R$ per day , "helicopter" => 1000
+const vfuel = Dict{String,Float64}(multivehicle ? ("MB" => 7/6, "VW" => 7/5) : "toco" => 7/4) # R$ per km , "helicopter" => 6.31
+const vspeed = Dict{String,Float64}(multivehicle ? ("MB" => 90, "VW" => 90) : "toco" => 80) #, "VW" => 70) # km/h
 
-totalWeight = 0
-for c in C
-    global totalWeight += weight[c] * sum(supply[c,t] for t in T)
-end
-const nK = totalWeight < ZERO ? 0 : totalWeight/(2*nT) <= 2040 ? 1 : 2*ceil(Int,totalWeight/(nV*(sum(values(vload))*nT-8150)))
-aux = ceil(Int, nK/2)
+#nKdict = Dict([(1,2), (2,2), (3,2), (4,1), (5,3), (6,3), (7,3), (8,2), (9,3), (10,1), (11,4), (12,2), (13,2), (14,2), (15,2), (16,5), (17,3), (18,3), (19,2), (20,3), (21,4), (22,3), (23,7), (24,7), (25,5), (26,8), (27,6), (28,5), (29,6), (30,4), (31,4), (32,8), (33,7), (34,6), (35,6), (191,1), (192,1), (193,1), (194,1), (195,1), (196,1), (197,1), (198,1), (199,1), (200,1), (201,2), (202,3), (203,2), (204,3), (205,3), (206,2), (207,3), (208,4), (209,3), (210,3), (211,3), (212,3), (213,2), (214,5), (215,4), (216,5), (217,4), (218,3), (219,4), (220,4), (221,4), (222,5), (223,7), (224,7), (225,4)])
+nKdict = Dict([(1,1), (2,1), (3,2), (4,1), (5,1), (6,1), (7,1), (8,2), (9,1), (10,1), (11,4), (12,1), (13,1), (14,2), (15,1), (16,3), (17,2), (18,3), (19,2), (20,2), (21,4), (22,3), (23,5), (24,5), (25,3), (26,7), (27,5), (28,4), (29,4), (30,4), (31,3), (32,7), (33,6), (34,5), (35,6), (191,1), (192,1), (193,1), (194,1), (195,1), (196,1), (197,1), (198,1), (199,1), (200,1), (201,2), (202,2), (203,2), (204,3), (205,2), (206,2), (207,3), (208,3), (209,2), (210,3), (211,3), (212,3), (213,2), (214,4), (215,3), (216,3), (217,4), (218,3), (219,4), (220,4), (221,4), (222,4), (223,5), (224,5), (225,4)])
+nK = get(nKdict, ninstance, 0) - pvehicles
+#nK = max(round(Int,(ninstance > 190 ? 1 : 1.5)*18.5*total_demand/(2.5*sum(values(vload))*nT)), (multivehicle && psupply > 0.99 && ninstance > 200 ? 2 : 1)) - 
+	#pvehicles + (multivehicle && (ninstance in [204,207,208,210,211,212] || ninstance > 213) ? 1 : 0)
+# aux = ninstance > 211 ? ceil(Int, nK/2) : floor(Int, nK/2)
+aux = floor(Int, nK/2)
 for k in 1:nK
     push!(K, string("k",k))
-    vmodel[K[k]] = totalWeight/(2*nT) <= 2040 || k > aux ? "MB" : "VW"
+    vmodel[K[k]] = multivehicle ? ((nK == 1 && ninstance in [194,195]) || k <= aux ? "VW" : "MB") : "toco"
 end
 
 for t in T
@@ -176,8 +176,8 @@ for t in T
 end
 for t in T
     for k in K
-        vehicle[k,t] = vmodel[k] != "VW" || t != 1
-        nvehicles[vmodel[k],t] += vmodel[k] == "VW" && t == 1 ? 0 : 1
+        vehicle[k,t] = true #vmodel[k] != "VW" || t != 1
+        nvehicles[vmodel[k],t] += 1 # vmodel[k] == "VW" && t == 1 ? 0 : 1
     end
 end
 # println("nvehicles = ", sum(nvehicles[vtype,2] for vtype in vtypes), " nK = ", nK)
@@ -191,28 +191,47 @@ open(string(filepath, instance, "vehicle.txt"), "w") do f
     end
 end=#
 
-pie = Dict{Any,Float32}()
-filename = string(filepath, instance, "pied.txt")
-if readPie(filename)
-    local aux = readdlm(filename, '$')
-    for i in 1:trunc(Int, length(aux)/4)
-        pie[aux[i,1],aux[i,2],aux[i,3]] = aux[i,4]
-    end
-else
-    pie = getPi()
-    open(filename, "w") do f
-        for (key,value) in pie
-            write(f, "$(key[1])\$$(key[2])\$$(key[3])\$$value\n")
+distance = Dict{Any,Float64}()
+if !roadpi
+for i in 1:645
+    for j in 1:645
+        if cities[i] in N && cities[j] in N
+            for t in T
+                distance[cities[i],cities[j],t] = auxDistance[i,j]
+            end
         end
     end
 end
-distance = floydWarshall()
+max_distance = maximum(auxDistance)
+else
+filename = string(filepath, instance, "pied.txt")
+pie = Dict()
+if !readPie(filename)
+    println("ERROR: could not find ", filename)
+    exit(1)
+    # for i in 1:645
+    #     for j in 1:645
+    #         if cities[i] in N && cities[j] in N
+    #             for t in T
+    #                 distance[cities[i],cities[j]] = auxDistance[i,j]
+    #             end
+    #         end
+    #     end
+    # end
+    # pie = getPi()
+    # open(filename, "w") do f
+    #     for (key,value) in pie
+    #         write(f, "$(key[1])\$$(key[2])\$$(key[3])\$$value\n")
+    #     end
+    # end
+    # distance = floydWarshall()
+end
+end
 
-nP = min(trunc(Int, working_hours/(minDistance(distance)*2/maximum(values(vspeed)) + minimum(values(vloadtime)))) + 2, 6)
+nP = trunc(Int, working_hours/(minimum(distance[o,j,t] for j in DP for t in T)*2/maximum(values(vspeed)) + minimum(values(vloadtime)) + unloadtime)) + 2
 P = collect(1:nP)
-# @printf("T = %d, working_hours = %d, P = %d\n", nT, working_hours, nP)
+println("multicommodity ", multicommodity, ", pi ", roadpi, ", multivehicle ", multivehicle, ", pvehicles ", pvehicles, ", nK = ", nK, ", nP = ", nP)
 
-const wfairness = 100
 
 # const S = Array{String}(undef, 0)
 # const Sk = Dict{String,Array{String}}("MB" => Array{String}(undef, 0), "VW" => Array{String}(undef, 0))
@@ -221,5 +240,7 @@ const wfairness = 100
 # const columxvalue = Dict{Any, Float64}()
 
 # release memory and force garbage collector to run
-populationDP = auxv = nothing
+populationDP = auxv = nKdict = nothing
+GC.gc()
+flush(stdout)
 GC.gc()
